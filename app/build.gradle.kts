0452903114f8160a7f6c5dc5c3cab2b5ed1765bb
Elizabeth Martin Campos <me@elizabeth.sh>
2024-03-20 22:34:50 +0100

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,8 @@ androidComponents {
 	beforeVariants {
 
 		android.sourceSets.getByName(it.name) {
-			java.srcDir(buildDir.resolve("generated/source/proto/${it.name}/java"))
-			kotlin.srcDir(buildDir.resolve("generated/source/proto/${it.name}/kotlin"))
+			java.srcDir(layout.buildDirectory.dir("generated/source/proto/${it.name}/java"))
+			kotlin.srcDir(layout.buildDirectory.dir("generated/source/proto/${it.name}/kotlin"))
 		}
 	}
 }
@@ -98,9 +98,9 @@ protobuf {
 
 dependencies {
 	implementation("androidx.core:core-ktx:1.12.0")
-	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0-rc02")
+	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 	implementation("androidx.activity:activity-compose:1.8.2")
-	implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+	implementation(platform("androidx.compose:compose-bom:2024.03.00"))
 	implementation("androidx.compose.ui:ui")
 	implementation("androidx.compose.ui:ui-graphics")
 	implementation("androidx.compose.ui:ui-tooling-preview")
@@ -111,25 +111,25 @@ dependencies {
 	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-	androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
+	androidTestImplementation(platform("androidx.compose:compose-bom:2024.03.00"))
 	androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 	debugImplementation("androidx.compose.ui:ui-tooling")
 	debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-	implementation("androidx.navigation:navigation-compose:2.7.6")
-	implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0-rc02")
-	implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0-rc02")
+	implementation("androidx.navigation:navigation-compose:2.7.7")
+	implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+	implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
 	implementation("androidx.compose.material3:material3-window-size-class")
 
-	implementation("com.google.dagger:hilt-android:2.48.1")
+	implementation("com.google.dagger:hilt-android:2.49")
 	ksp("com.google.dagger:hilt-android-compiler:2.48.1")
-	implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+	implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
 	implementation("androidx.datastore:datastore:1.0.0")
 	implementation("com.google.protobuf:protobuf-kotlin-lite:3.25.1")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-	implementation("androidx.browser:browser:1.7.0")
+	implementation("androidx.browser:browser:1.8.0")
 
 	// Needed for:
 	// java.time when targeting API Level <26
