1caa50f6213b6f070c60d01fd6f3b897c469d9d2
Elizabeth Martin Campos <me@elizabeth.sh>
2024-05-16 14:07:49 +0200

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -118,10 +118,10 @@ sqldelight {
 }
 
 dependencies {
-	implementation("androidx.core:core-ktx:1.12.0")
+	implementation("androidx.core:core-ktx:1.13.1")
 	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-	implementation("androidx.activity:activity-compose:1.8.2")
-	implementation(platform("androidx.compose:compose-bom:2024.03.00"))
+	implementation("androidx.activity:activity-compose:1.9.0")
+	implementation(platform("androidx.compose:compose-bom:2024.05.00"))
 	implementation("androidx.compose.ui:ui")
 	implementation("androidx.compose.ui:ui-graphics")
 	implementation("androidx.compose.ui:ui-tooling-preview")
@@ -130,10 +130,10 @@ dependencies {
 	implementation("androidx.compose.material:material-icons-extended")
 	implementation("androidx.compose.material3:material3-window-size-class")
 	testImplementation("junit:junit:4.13.2")
-	testImplementation("junit:junit:4.12")
+	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-	androidTestImplementation(platform("androidx.compose:compose-bom:2024.03.00"))
+	androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
 	androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 	debugImplementation("androidx.compose.ui:ui-tooling")
 	debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -144,14 +144,13 @@ dependencies {
 	implementation("androidx.compose.material3:material3-window-size-class")
 
 	implementation("com.google.dagger:hilt-android:2.51.1")
-	ksp("com.google.dagger:hilt-android-compiler:2.51")
-	ksp("com.google.dagger:hilt-compiler:2.51")
+	ksp("com.google.dagger:hilt-android-compiler:2.51.1")
 	ksp("androidx.hilt:hilt-compiler:1.2.0")
 	implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
-	implementation("androidx.datastore:datastore:1.0.0")
-	implementation("com.google.protobuf:protobuf-kotlin-lite:3.25.2")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+	implementation("androidx.datastore:datastore:1.1.1")
+	implementation("com.google.protobuf:protobuf-kotlin-lite:4.26.1")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
 	implementation("androidx.browser:browser:1.8.0")
 
@@ -159,10 +158,10 @@ dependencies {
 	// java.time when targeting API Level <26
 	coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 
-	val ktorVersion = "2.3.6"
+	val ktorVersion = "2.3.11"
 	implementation("io.ktor:ktor-client-core:$ktorVersion")
 	implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 	implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
 	implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
 	implementation("io.ktor:ktor-client-auth:$ktorVersion")
@@ -172,15 +171,15 @@ dependencies {
 	implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
 	implementation("com.github.bumptech.glide:okhttp3-integration:4.16.0")
 	ksp("com.github.bumptech.glide:ksp:4.16.0")
-	implementation("com.vanniktech:blurhash:0.1.0")
+	implementation("com.vanniktech:blurhash:0.3.0")
 
 	// SQLDelight
-	implementation("app.cash.sqldelight:android-driver:2.0.1")
-	implementation("app.cash.sqldelight:coroutines-extensions:2.0.1")
+	implementation("app.cash.sqldelight:android-driver:2.0.2")
+	implementation("app.cash.sqldelight:coroutines-extensions:2.0.2")
 	implementation("com.google.code.gson:gson:2.10.1")
 
 	// Firebase
-	implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+	implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
 	implementation("com.google.firebase:firebase-analytics")
 	implementation("com.google.firebase:firebase-messaging")
 
